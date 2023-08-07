c242b7d5307b4135d4f666cdf13ee842d6574148
nilsmechtel <nils.mech@gmail.com>
2023-08-07 02:05:17 +0200

    update pathway.xlsx

--- R/plot_network.R
+++ R/plot_network.R
@@ -3,74 +3,126 @@ library(dplyr)
 library(ggplot2)
 library(ggrepel)
 
-pathway_file <- pathway()
 
-# Prepare network input ---------------------------------------------------
+pathway_file <- "/Users/nilsm/Library/CloudStorage/OneDrive-bwedu/Uni/HiWi/HiWi COS/Package - MetAlyzer/MetAlyzer/inst/extdata/pathway.xlsx" # MetAlyzer::pathway()
 
-## Read pathway file
-nodes <- openxlsx::read.xlsx(pathway_file, sheet="Nodes") %>%
-  select(Label,
-         x,
-         y,
-         Shape,
-         Metabolites)
+read_named_region <- function(file_path, named_region) {
+  full_sheet <- openxlsx::read.xlsx(
+    file_path,
+    sheet = 1,
+    colNames = FALSE,
+    skipEmptyRows = FALSE,
+    skipEmptyCols = FALSE,
+  )
+  full_sheet[nrow(full_sheet) + 1, ] <- NA
+  header <- colnames(openxlsx::read.xlsx(
+    file_path,
+    namedRegion = named_region
+  ))
+  coordinates <- lapply(header, function(col_name) {
+    data.frame(which(full_sheet == col_name, arr.ind = TRUE))
+  }) %>%
+    dplyr::bind_rows() %>%
+    dplyr::arrange(row, col) %>%
+    dplyr::group_by(row) %>%
+    dplyr::mutate(n = n()) %>%
+    dplyr::filter(n == length(header))
+
+  header_row <- unique(coordinates$row)
+  first_row <- header_row + 1
+  cols <- coordinates$col
+  df <- full_sheet[
+    first_row:nrow(full_sheet),
+    cols
+  ]
+  colnames(df) <- header
+  last_row <- min(which(rowSums(is.na(df)) == length(header))) - 1
+  df <- df[1:last_row, ]
+
+  if ("x" %in% header) {
+    df$x <- as.numeric(df$x)
+  }
+  if ("y" %in% header) {
+    df$y <- as.numeric(df$y)
+  }
+  if ("Radius" %in% header) {
+    df$Radius <- as.numeric(df$Radius)
+  }
+  rownames(df) <- NULL
+  return(df)
+}
+
+
+## Read network nodes, edges and annotations
+pathways <- read_named_region(pathway_file, "Pathways_Header")
+pathways$Label <- stringr::str_trim(pathways$Label)
+invalid_annotations <- which(
+  is.na(pathways$Label) |
+  duplicated(pathways$Label) |
+  is.na(pathways$x) |
+  is.na(pathways$y) |
+  is.na(pathways$Color)
+)
+if (length(invalid_annotations) > 0) {
+  # print warning and remove
+  cat("Warning: Removing", length(invalid_annotations), "invalid pathways.\n")
+  pathways <- pathways[-invalid_annotations, ]
+}
+rownames(pathways) <- pathways$Label
+
+nodes <- read_named_region(pathway_file, "Metabolites_Header")
 nodes$Label <- stringr::str_trim(nodes$Label)
-colnames(nodes)[1] <- "ID"
-colnames(nodes)[2] <- "Position_x"
-colnames(nodes)[3] <- "Position_y"
-nodes$Position_x <- nodes$Position_x / 10
-nodes$Position_y <- nodes$Position_y / 10
-nodes$Pathway <- NA
-nodes$Pathway[1:30] <- "Bile Acids"
+nodes$Pathway[is.na(nodes$Pathway)] <- ""
+invalid_nodes <- which(
+  is.na(nodes$Label) |
+  duplicated(nodes$Label) |
+  is.na(nodes$x) |
+  is.na(nodes$y) |
+  !nodes$Pathway %in% c(pathways$Label, "")
+)
+if (length(invalid_nodes) > 0) {
+  # print warning and remove
+  cat("Warning: Removing", length(invalid_nodes), "invalid nodes.\n")
+  nodes <- nodes[-invalid_nodes, ]
+}
+rownames(nodes) <- nodes$Label
 
-edges <- openxlsx::read.xlsx(pathway_file, sheet="Edges") %>%
-  select(Node1,
-         Node2,
-         Rad)
+edges <- read_named_region(pathway_file, "Connections_Header")
 edges$Node1 <- stringr::str_trim(edges$Node1)
 edges$Node2 <- stringr::str_trim(edges$Node2)
-colnames(edges)[1] <- "from_ID"
-colnames(edges)[2] <- "to_ID"
-colnames(edges)[3] <- "Curvature"
-edges$from <- sapply(edges$from_ID, function(id) {
-  which(nodes$ID == id)
-})
-edges$to <- sapply(edges$to_ID, function(id) {
-  which(nodes$ID == id)
-})
+invalid_edges <- which(
+  !edges$Node1 %in% nodes$Label |
+  !edges$Node2 %in% nodes$Label |
+  edges$Node1 == edges$Node2
+)
+if (length(invalid_edges) > 0) {
+  # print warning and remove
+  cat("Warning: Removing", length(invalid_edges), "invalid connections.\n")
+  edges <- edges[-invalid_edges, ]
+}
 
-pathways <- openxlsx::read.xlsx(pathway_file, sheet="Annotations") %>%
-  select(Annotation,
-         x,
-         y)
-pathways$Annotation <- stringr::str_trim(pathways$Annotation)
-colnames(pathways)[1] <- "Pathway"
-colnames(pathways)[2] <- "Position_x"
-colnames(pathways)[3] <- "Position_y"
-pathways$Position_x <- pathways$Position_x / 10
-pathways$Position_y <- pathways$Position_y / 10
-pathways$Color <- rownames(pathways)
 
-nodes <- filter(nodes,
-                !is.na(Position_x),
-                !is.na(Position_y))
-nodes$FC_thresh <- floor(runif(rownames(nodes), -3, 4)) #!
-
-nodes$Pathway[is.na(nodes$Pathway)] <- ""
-edges$Pathway <- sapply(seq_along(rownames(edges)), function(i) {
-  from <- edges[i, "from"]
-  to <- edges[i, "to"]
+edges$x_start <- nodes[edges$Node1, "x"]
+edges$y_start <- nodes[edges$Node1, "y"]
+edges$x_end <- nodes[edges$Node2, "x"]
+edges$y_end <- nodes[edges$Node2, "y"]
+edges$Pathway <- sapply(rownames(edges), function(rowname) {
+  from <- edges[rowname, "Node1"]
+  to <- edges[rowname, "Node2"]
   from_pathway <- nodes[from, "Pathway"]
   to_pathway <- nodes[to, "Pathway"]
-  color <- ifelse(all(c(from_pathway == to_pathway),
-                      from_pathway %in% pathways$Pathway),
-                  pathways$Color[which(pathways$Pathway == from_pathway)],
-                  "grey")
-                  return(color)
+  color <- "grey"
+  if (from_pathway == to_pathway & from_pathway != "") {
+    color <- pathways[from_pathway, "Color"]
+  }
+  return(color)
 })
 
-# Draw network ------------------------------------------------------------
 
+## Add log2FC to nodes
+nodes$FC_thresh <- floor(runif(rownames(nodes), -3, 4)) # !
+
+## Draw network
 
 # Create a plot of the network using ggplot2 and ggrepel
 label_size <- 3
@@ -79,46 +131,63 @@ edge_size <- 1
 annotation_size <- 6
 
 network <- ggplot()
-for (curvature in unique(edges$Curvature)) {
-  tmp_edges <- filter(edges, Curvature == curvature)
+for (radius in unique(edges$Radius)) {
+  tmp_edges <- filter(edges, Radius == radius)
+
   network <- network +
     # Add the round area behind the edges
-    geom_curve(data = tmp_edges,
-               aes(x = nodes[from, "Position_x"],
-                   y = nodes[from, "Position_y"],
-                   xend = nodes[to, "Position_x"],
-                   yend = nodes[to, "Position_y"],
-                   color = Pathway),
-               # color = "lightblue",
-               size = area_size,
-               alpha=0.3,
-               curvature = curvature,
-               show.legend = FALSE) +
+    geom_curve(
+      data = tmp_edges,
+      aes(
+        x = x_start,
+        y = y_start,
+        xend = x_end,
+        yend = y_end,
+        color = Pathway
+      ),
+      # color = "lightblue",
+      size = area_size,
+      alpha = 0.3,
+      curvature = radius,
+      show.legend = FALSE
+    ) +
     # Or add the edges as curved lines
-    geom_curve(data = tmp_edges,
-               aes(x = nodes[from, "Position_x"],
-                   y = nodes[from, "Position_y"],
-                   xend = nodes[to, "Position_x"],
-                   yend = nodes[to, "Position_y"]),
-               color = "grey", size = edge_size, curvature = curvature)
+    geom_curve(
+      data = tmp_edges,
+      aes(
+        x = nodes[Node1, "x"],
+        y = nodes[Node1, "y"],
+        xend = nodes[Node2, "x"],
+        yend = nodes[Node2, "y"]
+      ),
+      color = "grey", size = edge_size, curvature = radius
+    )
 }
 network <- network +
   # Add labels at the position of the nodes
-  geom_label(data = nodes,
-             aes(x = Position_x,
-                 y = Position_y,
-                 label = ID,
-                 fill = FC_thresh),
-             size = label_size,
-             color = "white") +
+  geom_label(
+    data = nodes,
+    aes(
+      x = x,
+      y = y,
+      label = Label,
+      fill = FC_thresh
+    ),
+    size = label_size,
+    color = "white"
+  ) +
   # Add annotations
-  geom_text(data = pathways,
-            aes(x = Position_x,
-                y = Position_y,
-                label = Pathway,
-                color = Color),
-            size = annotation_size,
-            show.legend = FALSE) +
+  geom_text(
+    data = pathways,
+    aes(
+      x = x,
+      y = y,
+      label = Label,
+      color = Color
+    ),
+    size = annotation_size,
+    show.legend = FALSE
+  ) +
   # # Set the x and y axis limits
   # xlim(0, 10) +
   # ylim(0, 10) +
@@ -127,3 +196,5 @@ network <- network +
   ggtitle("Example Network Plot with Colored Area Behind Curved Edges") +
   theme(plot.title = element_text(hjust = 0.5))
 network
+
+ggsave("network.png", network, width = 15, height = 10, bg = "white")
